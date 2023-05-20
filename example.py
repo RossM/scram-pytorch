72c7970b041edc8522dc8748f9d31009bd0c0edb
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2023-05-20 05:17:11 -0700

    Update example.py

--- example.py
+++ example.py
@@ -6,19 +6,18 @@ def optimize(inputs, target):
     p = nn.Parameter(torch.zeros([inputs.shape[1]], dtype=torch.float32))
     loss_fn = nn.MSELoss()
     
-    optimizer = Scram([p], lr=0.5)
+    optimizer = Scram([p], lr=0.5, weight_decay=0.1)
+    
+    for step in range(100):
+        optimizer.zero_grad()
+        pred = torch.sigmoid(torch.einsum('y x, x -> y', inputs, p))
+        loss = loss_fn(pred, target)
+        loss.backward()
+        optimizer.step()
 
     pred = torch.sigmoid(torch.einsum('y x, x -> y', inputs, p))
     loss = loss_fn(pred, target)
-    
-    for step in range(11):
-        print(f"step {step}:")
-        print(f"p={p.data}\nerr={torch.abs(pred - target).detach()}\nloss={loss}\n")
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        pred = torch.sigmoid(torch.einsum('y x, x -> y', inputs, p))
-        loss = loss_fn(pred, target)
+    print(f"p={p.data}\nerr={torch.abs(pred - target).detach()}\nloss={loss}\n")
 
 def main():
     inputs = torch.tensor([[0, 1, 0, 1],
