dd11498d6ce13400cd8b36079b69dc166865f171
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2023-05-20 19:55:46 -0700

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'scram-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.1',
+  version = '0.1.0',
   license='MIT',
   description = 'Scram Optimizer - Pytorch',
   author = 'Ross Morgan-Linial',
