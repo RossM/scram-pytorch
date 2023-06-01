1223048803ef4863ad9eb210f1d88a8940104e1a
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2023-05-31 20:17:42 -0700

    Squashed commit of the following:
    commit d23985061d2e5ce43c67a44204a0c7bd2e01b9a6
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 20:13:01 2023 -0700
    
        More info about algorithm
    
    commit 5cb0ae2795fdcef95ae868fca23fba2b72e77049
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 20:09:49 2023 -0700
    
        Add comments
    
    commit 8fb32f786517089eb799e3f3f7bc22c343c27a38
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 16:04:35 2023 -0700
    
        Remove --autolr
    
    commit 2f489186ba0aae0323321b984e401a18532283bd
    Merge: 5e9e83c 8d06a21
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 16:02:36 2023 -0700
    
        Merge branch 'main' into dev
    
    commit 5e9e83c75370622b0c5734489e7906fc686e9d0d
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 14:44:32 2023 -0700
    
        Fix
    
    commit eee2dbaec02e837c4dc7eef6eba9a8ac58e678bf
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 14:05:37 2023 -0700
    
        Make AutoLR its own class
    
    commit ebe99eccd142ef06cb19b7eec01acecee5006472
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Wed May 31 06:44:03 2023 -0700
    
        Revert "Add damping"
    
        This reverts commit a55c5d7475655c50505589df51149e2889cac99b.
    
    commit a55c5d7475655c50505589df51149e2889cac99b
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 17:18:29 2023 -0700
    
        Add damping
    
    commit 2b808080720e673eedcb1253c305da00b8036316
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 17:15:36 2023 -0700
    
        Cleanup
    
    commit 3e88ac12e6c0227d9e13c6bf400fd4c50834c0b5
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 14:56:14 2023 -0700
    
        Autolr overhaul
    
    commit 6c99d66d6d23e5a56a38b00e2eaebd676bbf359e
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 14:20:14 2023 -0700
    
        Slight adjustment to autolr
    
    commit cb8bcf6a800a22dfc38c98914284e584b46f1177
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 14:07:41 2023 -0700
    
        Fix
    
    commit a51147f0ac52d9d9f29cca78d37773dcb5908958
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 13:59:42 2023 -0700
    
        Fix
    
    commit 8f426e443de4cf9702f084d446b3f1d6e449c61b
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 13:53:52 2023 -0700
    
        Hack for testing
    
    commit cffa275c53e232841f96856c787ee4be6ad7c50c
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 13:48:04 2023 -0700
    
        Fix
    
    commit f3207f9387d0c38d871e161c757950c2099aaa4a
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 13:41:40 2023 -0700
    
        Fix
    
    commit b1680a3ee4a5a4d09ad96ba5b3bdbef3cb14e4dc
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 13:09:09 2023 -0700
    
        Fix
    
    commit cf3bb9c9d883e74b14dfa815be16c07aa4719194
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 12:55:46 2023 -0700
    
        Fix
    
    commit dabb9408419bbf788adf852891eb38325f2d06e9
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Tue May 30 12:46:01 2023 -0700
    
        Add autolr
    
    commit 3500af3c474c0061d43c1908a1081ea30d3b492a
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Mon May 29 03:19:26 2023 -0700
    
        Add normalize option
    
    commit 95bd2ff4c1938092e1ee357bc36d58c479cfcf17
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Fri May 26 18:18:13 2023 -0700
    
        Add layerwise option to Simon
    
    commit a96b8693fad842bbe05e67efe8f2fe3c237d33e2
    Author: Ross Morgan-Linial <ross.morgan.linial@gmail.com>
    Date:   Fri May 26 12:38:17 2023 -0700
    
        Temp fix

--- scram_pytorch/autolr.py
+++ scram_pytorch/autolr.py
@@ -3,6 +3,18 @@ import torch, numpy
 from torch.optim.lr_scheduler import LRScheduler
 
 class AutoLR:
+    """
+    A learning rate scheduler that automatically adjusts learning rate by using a zeroth-order
+    optimization algorithm to maximize the rate of descent of loss.
+    
+    The algorithm adds a slight random variation to the learning rate each optimization step.
+    It then calculates the covariance between the random value and the change in loss that step,
+    and uses the covariance to determine how to adjust learning rate to increase the change
+    in loss.
+    
+    In practice this may underperform a carefully hand-tuned learning rate.
+    """
+    
     def __init__(
         self,
         optimizer,
@@ -41,29 +53,44 @@ class AutoLR:
 
         self.steps += 1
 
+        # Track exponential moving averages of the 1st and 2nd moments of learning rate and loss delta
         self.exp_lr = autolr_beta * self.exp_lr + (1 - autolr_beta) * self.last_rand_lr
         self.exp_lr_sq = autolr_beta * self.exp_lr_sq + (1 - autolr_beta) * self.last_rand_lr ** 2
         self.exp_loss = autolr_beta * self.exp_loss + (1 - autolr_beta) * loss_delta
         self.exp_loss_sq = autolr_beta * self.exp_loss_sq + (1 - autolr_beta) * loss_delta ** 2
 
+        # Calculate standard deviations of learning rate and loss delta
         lr_stdev = (self.exp_lr_sq - self.exp_lr ** 2) ** 0.5 or 1
         loss_stdev = (self.exp_loss_sq - self.exp_loss ** 2) ** 0.5 or 1
 
+        # Calculate covariance of learning rate and loss delta
         cov = (lr_diff / lr_stdev) * (loss_diff / loss_stdev)
+        
+        # Track exponential moving average of covariance
         self.exp_cov = autolr_beta * self.exp_cov + (1 - autolr_beta) * cov
+        
+        # Add a bit of damping to the exponential moving average to avoid periodic oscillation
         update = self.betas[0] * self.exp_cov + (1 - self.betas[0]) * cov
 
+        # Update learning rate based on covariance
         self.lr_mult *= math.exp(-self.adjustment_rate * update * self.betas[1])
 
         #print(f"self.exp_lr={self.exp_lr}, self.exp_loss={self.exp_loss}, self.exp_cov={self.exp_cov}, self.lr_mult={self.lr_mult}")
             
+        # Select a learning rate for the next step
+        # We deliberately only track the random part of the learning rate for calculating the adjustment.
+        # Including the adjustment in the covariance calculations creates correlations over time that
+        # can cause feedback effects and divergence.
         rand_lr = random.uniform(0.9, 1.1)
         
+        # Save data for next step
         self.last_loss = loss
         self.last_rand_lr = rand_lr
         
+        # Calculate learning rates for each parameter group
         self._last_lr = [base_lr * rand_lr * self.lr_mult for base_lr in self.base_lrs]
         
+        # Update optimizer learning rates
         for i, data in enumerate(zip(self.optimizer.param_groups, self._last_lr)):
             param_group, lr = data
             param_group['lr'] = lr
